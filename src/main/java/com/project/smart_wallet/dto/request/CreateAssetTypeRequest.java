dcc90cf62c0ebef49bd509b5971884816de1320c
Alexsandro <alexsandro.carvalho@cwi.com.br>
2026-05-12 19:54:56 -0300

    refactor: remove dto sem uso

--- src/main/java/com/project/smart_wallet/dto/request/CreateAssetTypeRequest.java
+++
@@ -1,7 +0,0 @@
-package com.project.smart_wallet.dto.request;
-
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-
-public record CreateAssetTypeRequest(@NotBlank String name, @NotNull Boolean incomeGenerating) {
-}
